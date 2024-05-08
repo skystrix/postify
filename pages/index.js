79614010690c6aff05307723d63a3d75761a961d
Santanu Das <santanud.411@gmail.com>
2024-05-09 02:13:04 +0530

    initial changes and adding dependencies

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+// pages/index.js
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to My Next.js Project</h1>
+    </div>
+  );
+};
+
+export default Home;
